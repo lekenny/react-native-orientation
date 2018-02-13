1a58cb812ebd51b59d92c96adaa3ec71a3ab2f18
聂先生 <572545017@qq.com>
2018-02-13 16:23:52 +0800

    Update Orientation.h
    "RCTBridgeModule.h" => <React/RCTBridgeModule.h>

--- iOS/RCTOrientation/Orientation.h
+++ iOS/RCTOrientation/Orientation.h
@@ -7,7 +7,7 @@
 #if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
 #else
-#import "RCTBridgeModule.h"
+#import <React/RCTBridgeModule.h>
 #endif
 
 @interface Orientation : NSObject <RCTBridgeModule>
